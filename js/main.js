393b247a49fb7a650050b22714601802617093cd
Greg Miller <shrinkfolio@gmail.com>
2016-03-30 08:36:36 -0400

    Optimize images, add js nav functionality, content revised

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-
-
-
-
-
-// back to top script
-
 jQuery(document).ready(function () {
+    
+// script: back to top script
+    
   // show or hide the sticky footer button
   jQuery(window).scroll(function () {
     if (jQuery(this).scrollTop() > 200) {
@@ -20,6 +16,36 @@ jQuery(document).ready(function () {
     event.preventDefault();
 
     jQuery('html, body').animate({scrollTop: 0}, 400);
-  });
+  }); //  scroll to top function
+});
 
-}); // go top
+ // script: scroll from top nav to hash section
+ // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+  jQuery(document).ready(function () {  
+      jQuery(function() {
+      jQuery('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+          var target = jQuery(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            jQuery('html, body').animate({
+              scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+        }); // scroll to hash
+    });
+    
+
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function (event) {
+      event.preventDefault();
+      $(".navbar-collapse").collapse('hide');
+    
+  });
+});
+
